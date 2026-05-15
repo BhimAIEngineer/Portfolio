772f9ed11b20e222c0a84240c89e0256c8ef7fc4
BhimAIEngineer <bhimrajbhar.ai.engineer@gamil.com>
2026-05-15 22:12:49 +0530

    Add remaining project folders

---
+++ Python/Calculator-App/README.md
@@ -0,0 +1,14 @@
+# 🧮 Calculator App
+
+A simple calculator application built using Python.
+
+## 🚀 Features
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+## ▶️ Run the Program
+
+```bash
+python calculator.py
---
+++ Python/Calculator-App/calculator.py
@@ -0,0 +1,31 @@
+# Simple Calculator App in Python
+
+print("Simple Calculator")
+
+# Taking input from user
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Performing calculations
+if operator == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif operator == "-":
+    result = num1 - num2
+    print("Result:", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("Result:", result)
+
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error! Division by zero.")
+
+else:
+    print("Invalid operator")
